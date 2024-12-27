62f0cbe623ea0619238756b6c24e45fe4b521b37
Tof Onaeko <tonaeko@hotmail.co.uk>
2024-12-27 18:10:04 +0000

    tof-1: correctly inished task and obtained the correct answer

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -5,22 +5,42 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Application {
 
-    private Map<String, ArrayList<Integer>> arrays = new HashMap<>();
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private static Map<String, ArrayList<Integer>> arrays = new HashMap<>();
     private static Application application = new Application();
 
     public static void main( String[] args) throws JsonProcessingException {
-        application.createArray("numbers1.json");
-        application.createArray("numbers2.json");
+        application.setup();
+
+        ArrayList<Integer> list1 = arrays.get("1");
+        ArrayList<Integer> list2 = arrays.get("2");
+        Collections.sort(list1);
+        Collections.sort(list2);
+
+        System.out.println(application.calculateDifference(list1, list2));
     }
 
-    public ArrayList<Integer> createArray(String json) throws JsonProcessingException {
+    private AtomicInteger calculateDifference(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        AtomicInteger index = new AtomicInteger(0);
+        AtomicInteger overallDifference = new AtomicInteger();
+
+        list1.stream().forEach(num -> {
+            int currentIndex = index.getAndIncrement();
+            int list2Value = list2.get(currentIndex);
+            int difference = num - list2Value;
+            if (difference < 0) {difference = difference * -1;}
+            overallDifference.addAndGet(difference);
+        });
+
+        return overallDifference;
+    }
+
+    private ArrayList<Integer> createArray(String json) throws JsonProcessingException {
         ArrayList<Integer> list = new ArrayList<>();
 
         try {
@@ -40,6 +60,11 @@ public class Application {
 
         arrays.put(String.valueOf(arrays.size() + 1), list);
         return list;
+    }
+
+    private void setup() throws JsonProcessingException {
+        application.createArray("numbers1.json");
+        application.createArray("numbers2.json");
     }
 
 }
